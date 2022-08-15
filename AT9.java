7a46f7de53edbfac0569beb0f4be509a0b7b8519
vidalreis92 <100246272+vidalreis92@users.noreply.github.com>
2022-08-14 23:11:53 -0300

    Create AT9.java

---
+++ AT9.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.lista3;
+
+/**
+ *
+ * @author Vidal Reis
+ */
+public class AT9 {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 50; i = i + 2) {
+            System.out.print(i + " ");
+        }
+    }
+}
